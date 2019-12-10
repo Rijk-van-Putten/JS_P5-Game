04a156c53f66ab3d2a877e4f36d2f3d7590d77e9
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-12-10 11:10:24 +0000

    df;sjak

---
+++ p5/game01/main.js
@@ -0,0 +1,12 @@
+function setup() {
+  var canvas;
+  createCanvas(450,450);
+  frameRate(60);
+}
+
+function draw() {
+background(240,240,240);
+    noStroke();
+  fill(250,225,175);
+  triangle(0,150,450,0,300,450);
+}
