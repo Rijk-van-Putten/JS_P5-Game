f4353229875db4aac4c061f7770c54b6149066e6
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-12-16 15:09:01 +0000

    movement

--- p5/game01/main.js
+++ p5/game01/main.js
@@ -1,12 +1,53 @@
-function setup() {
-  var canvas;
-  createCanvas(450,450);
-  frameRate(60);
+const CANVAS_WIDTH = 1200;
+const CANVAS_HEIGHT = 800;
+const MOVE_SPEED = 4;
+var position;
+var bullets
+
+
+class Bullet
+{
+    constructor
+    {
+        this.x = 0;
+        this.y = 0;
+    }
 }
 
+
+function setup() {
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  frameRate(60);
+  position = createVector(100, 100);
+}
+
+
 function draw() {
-background(240,240,240);
+    var horizontalInput = 0;
+    if (keyIsDown(LEFT_ARROW))
+        horizontalInput = -1;
+    else if (keyIsDown(RIGHT_ARROW))
+        horizontalInput = 1;
+    else
+        horizontalInput = 0;
+    var verticalInput = 0;
+    if (keyIsDown(DOWN_ARROW))
+        verticalInput = -1;
+    else if (keyIsDown(UP_ARROW))
+        verticalInput = 1;
+    else
+        verticalInput = 0;
+
+    var movement = createVector(horizontalInput, -verticalInput);
+    movement.normalize();
+    movement.mult(MOVE_SPEED);
+    position.add(movement);
+    background(240,240,240);
     noStroke();
-  fill(250,225,175);
-  triangle(0,150,450,0,300,450);
+    fill(255, 50, 50);
+    ellipse(position.x, position.y, 64);
+}
+
+function mousePressed() {
+
 }
