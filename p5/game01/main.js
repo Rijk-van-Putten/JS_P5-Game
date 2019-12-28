0bb4bf630b3c1505f959341f337916567fe4da1d
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-12-28 17:54:19 +0100

    Physics wip

--- p5/game01/main.js
+++ p5/game01/main.js
@@ -1,82 +1,84 @@
 const CANVAS_WIDTH = 900;
 const CANVAS_HEIGHT = 800;
 const FRAME_RATE = 60;
-const DELTA_TIME = 1/FRAME_RATE;
+const DELTA_TIME = 1 / FRAME_RATE;
 const MOVE_SPEED = 4;
+const BULLET_SIZE = 12;
 const BULLET_SPEED = 6;
 const FIRE_DELAY = 0.04;
 const ENEMY_SIZE = 64;
+const ENEMY_SPEED = 2;
 const PLAYER_SIZE = 64;
 
 var playerPos;
-var bullets = [ ];
-var enemies = [ ];
+var bullets = [];
+var enemies = [];
 var fireCountdown = 0;
 
-class Bullet
-{
-    constructor(position, direction)
-    {
+class Bullet {
+    constructor(position, direction) {
         this.position = position;
         this.direction = direction;
     }
 
-    update()
-    {
-        this.position.add(this.direction.copy().mult(BULLET_SPEED));
+    update() {
+        var movement = this.direction.copy().mult(BULLET_SPEED);
+        this.position.add(movement);
+        var enemy = checkPhysics(this.position, BULLET_SIZE);
+        if (enemy != null) {
+            var index1 = enemies.indexOf(enemy);
+            if (index1 >= 0)
+                enemies.splice(index1, 1); 
+            var index2 = bullets.indexOf(this);
+            if (index2 >= 0)
+                bullets.splice(index2, 1);
+        }
     }
 
-    draw()
-    {
-        fill('black');
-        ellipse(this.position.x, this.position.y, 12);
+    draw() {
+        fill('orange');
+        rect(this.position.x, this.position.y, BULLET_SIZE, BULLET_SIZE);
     }
 }
 
-class Enemy
-{
-    constructor(position)
-    {
+class Enemy {
+    constructor(position) {
         this.position = position;
     }
-    
-    update(playerPosition)
-    {
+
+    update(playerPosition) {
         var movement = playerPos.copy().sub(this.position);
         movement.normalize();
-        if (checkPhysics(this, this.position.copy().add(createVector(movement.x, 0))))
-        {
+        movement.mult(ENEMY_SPEED);
+        if (checkPhysics(this, this.position.copy().add(createVector(movement.x, 0)), ENEMY_SIZE, true)) {
             movement.x = 0;
         }
-        if (checkPhysics(this, this.position.copy().add(createVector(0, movement.y))))
-        {
+        if (checkPhysics(this, this.position.copy().add(createVector(0, movement.y)), ENEMY_SIZE, true)) {
             movement.y = 0;
         }
         this.position.add(movement);
     }
-    
-    draw()
-    {
+
+    draw() {
         rectMode(CENTER);
         fill('brown');
         rect(this.position.x, this.position.y, ENEMY_SIZE, ENEMY_SIZE);
     }
 
-    getPos()
-    {
+    getPos() {
         return this.position;
     }
 }
 
 function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  frameRate(60);
-  playerPos = createVector(100, 100);
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    frameRate(60);
+    playerPos = createVector(100, 100);
 
-  enemies.push(new Enemy(createVector(200, 300)));
-  enemies.push(new Enemy(createVector(700, 500)));
-  enemies.push(new Enemy(createVector(500, 300)));
-  enemies.push(new Enemy(createVector(600, 900)));
+    enemies.push(new Enemy(createVector(200, 300)));
+    enemies.push(new Enemy(createVector(700, 500)));
+    enemies.push(new Enemy(createVector(500, 300)));
+    enemies.push(new Enemy(createVector(600, 900)));
 
 }
 
@@ -96,61 +98,61 @@ function draw() {
         verticalInput = 1;
     else
         verticalInput = 0;
-        
+
     // PLAYER MOVEMENT
     var movement = createVector(horizontalInput, -verticalInput);
     movement.normalize();
     movement.mult(MOVE_SPEED);
+    if (checkPhysics(this, playerPos.copy().add(createVector(movement.x, 0)), PLAYER_SIZE, false)) {
+        movement.x = 0;
+    }
+    if (checkPhysics(this, playerPos.copy().add(createVector(0, movement.y)), PLAYER_SIZE, false)) {
+        movement.y = 0;
+    }
     playerPos.add(movement);
-    
+
     if (fireCountdown > 0) {
         fireCountdown -= DELTA_TIME;
     }
     else if (mouseIsPressed && mouseButton == LEFT) {
         fire();
     }
-    
-    background(240,240,240);
+
+    background(240, 240, 240);
     updateBullets();
     updateEnemies();
-    
-    drawBullets();
-    drawEnemies();
-    noStroke();
-    fill(255, 50, 50);
-    rectMode(CENTER);
 
+    drawEnemies();
+
+    noStroke();
+    rectMode(CENTER);
+    fill(255, 50, 50);
     rect(playerPos.x, playerPos.y, PLAYER_SIZE, PLAYER_SIZE);
+
+    drawBullets();
+
 }
 
-function updateEnemies()
-{
-    for(var i = 0; i < enemies.length; i++)
-    {
+function updateEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
         enemies[i].update(playerPos);
     }
 }
 
-function drawEnemies()
-{
-    for(var i = 0; i < enemies.length; i++)
-    {
+function drawEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
         enemies[i].draw();
     }
 }
 
-function updateBullets()
-{
-    for(var i = 0; i < bullets.length; i++)
-    {
+function updateBullets() {
+    for (var i = 0; i < bullets.length; i++) {
         bullets[i].update();
     }
 }
 
-function drawBullets()
-{
-    for(var i = 0; i < bullets.length; i++)
-    {
+function drawBullets() {
+    for (var i = 0; i < bullets.length; i++) {
         bullets[i].draw();
     }
 }
@@ -163,49 +165,79 @@ function fire() {
     bullets.push(new Bullet(createVector(playerPos.x, playerPos.y), direction));
 }
 
-class AABB
-{
-    constructor(x1, x2, y1, y2)
-    {
+class AABB {
+    constructor(object, x1, x2, y1, y2) {
+        this.object = object;
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
-
     }
 }
 
-function checkPhysics(self, position)
-{
-    var colliders = [ ];
-    for(var i = 0; i < enemies.length; i++)
-    {
+function checkPhysics(self, position, size, collideWithPlayer) {
+    var colliders = [];
+    for (var i = 0; i < enemies.length; i++) {
         if (self == enemies[i])
             continue;
 
-        colliders.push(new AABB(enemies[i].getPos().x - ENEMY_SIZE/2, enemies[i].getPos().x + ENEMY_SIZE/2, 
-                    enemies[i].getPos().y - ENEMY_SIZE/2, enemies[i].getPos().y + ENEMY_SIZE/2));
+        colliders.push(new AABB(enemies[i], enemies[i].getPos().x - ENEMY_SIZE / 2, enemies[i].getPos().x + ENEMY_SIZE / 2,
+            enemies[i].getPos().y - ENEMY_SIZE / 2, enemies[i].getPos().y + ENEMY_SIZE / 2));
     }
-  //  colliders.push(new AABB(playerPos.x - PLAYER_SIZE/2, playerPos.x + PLAYER_SIZE/2, 
- //                           playerPos.y - PLAYER_SIZE/2, playerPos.y + PLAYER_SIZE/2));
-    
+    if (collideWithPlayer) {
+        colliders.push(new AABB(null, playerPos.x - PLAYER_SIZE / 2, playerPos.x + PLAYER_SIZE / 2,
+            playerPos.y - PLAYER_SIZE / 2, playerPos.y + PLAYER_SIZE / 2));
+    }
+
     var points = [];
-    points.push(createVector(position.x + 32, position.y + 32));
-    points.push(createVector(position.x + 32, position.y - 32));
-    points.push(createVector(position.x - 32, position.y + 32));
-    points.push(createVector(position.x - 32, position.y - 32));
-    
-    for(var i = 0; i < colliders.length; i++)
-    {
-        for(var j = 0; j < points.length; j++)
-        {
-            if (points[j].x >= colliders[i].x1 && points[j].x <= colliders[i].x2
-                && points[j].y >= colliders[i].y1 && points[j].y <= colliders[i].y2);
-            {
-                return true;
+    points.push(createVector(position.x + size / 2, position.y + size / 2));
+    points.push(createVector(position.x + size / 2, position.y - size / 2));
+    points.push(createVector(position.x - size / 2, position.y + size / 2));
+    points.push(createVector(position.x - size / 2, position.y - size / 2));
+
+    for (var j = 0; j < colliders.length; j++) {
+        for (var k = 0; k < points.length; k++) {
+            if (points[k].x >= colliders[j].x1 && points[k].x <= colliders[j].x2) {
+                if (points[k].y >= colliders[j].y1 && points[k].y <= colliders[j].y2) {
+                    return true;
+                }
             }
         }
     }
-   
+
     return false;
+}
+
+// function checkPhysics(position, size) {
+//     var colliders = [];
+//     for (var i = 0; i < enemies.length; i++) {
+//         colliders.push(new AABB(enemies[i], enemies[i].getPos().x - ENEMY_SIZE / 2, enemies[i].getPos().x + ENEMY_SIZE / 2,
+//             enemies[i].getPos().y - ENEMY_SIZE / 2, enemies[i].getPos().y + ENEMY_SIZE / 2));
+//     }
+
+//     var points = [];
+//     points.push(createVector(position.x + size / 2, position.y + size / 2));
+//     points.push(createVector(position.x + size / 2, position.y - size / 2));
+//     points.push(createVector(position.x - size / 2, position.y + size / 2));
+//     points.push(createVector(position.x - size / 2, position.y - size / 2));
+
+//     for (var j = 0; j < colliders.length; j++) {
+//         for (var k = 0; k < points.length; k++) {
+//             if (points[k].x >= colliders[j].x1 && points[k].x <= colliders[j].x2) {
+//                 if (points[k].y >= colliders[j].y1 && points[k].y <= colliders[j].y2) {
+//                     return colliders[j].object;
+//                 }
+//             }
+//         }
+//     }
+
+//     return null;
+// }
+
+function drawColliders(colliders) {
+    for (var i = 0; i < colliders.length; i++) {
+        rectMode(CORNER);
+        fill("purple");
+        rect(colliders[i].x1, colliders[i].y1, colliders[i].x2 - colliders[i].x1, colliders[i].y2 - colliders[i].y1);
+    }
 }
